f46e8528bc8cbec5b71dfc8bdf78a995c980b13e
Moomi98 <kymkjh2002@gmail.com>
2023-09-04 20:25:54 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  </head>
+  <Mdi icon="delete" :size="500" />
+  <Mdi icon="face" />
+</template>
+<script setup lang="ts">
+import Mdi from '@/mdi/Mdi.vue'
+</script>
---
+++ src/mdi/Mdi.vue
@@ -0,0 +1,40 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  />
+  <span class="material-icons icon md-48">
+    {{ props.icon }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  icon: { type: String, required: true },
+  width: { type: [String, Number], default: 30 },
+  height: { type: [String, Number], default: 30 },
+  size: { type: Number, default: 24 }
+})
+
+const iconWidth = computed(() => {
+  const width = props.width
+  return typeof width === 'number' ? `${width}px` : width
+})
+
+const iconHeight = computed(() => {
+  const height = props.height
+  return typeof height === 'number' ? `${height}px` : height
+})
+</script>
+<style scoped>
+.material-icons {
+  font-size: v-bind('`${props.size}px`');
+  font-variation-settings:
+    'FILL' 1,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24;
+}
+</style>
